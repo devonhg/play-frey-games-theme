9370b00d79ae7c649bd71fc200530062f9faa770
devonhg <devon.hugh.godfrey@gmail.com>
2015-06-03 00:08:08 -0400

    Got header started

--- header.php
+++ header.php
@@ -23,21 +23,23 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', '_s' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php if ( is_active_sidebar( 'header-area' ) ) { ?>
-				<div class="sidebar-branding" role="complementary">
-					<?php dynamic_sidebar( 'header-area' ); ?>
-				</div><!-- #primary-sidebar -->
-			<?php } else { ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-			<?php } ?>
-		</div><!-- .site-branding -->
+		<div class='wrapper-line'>
+			<div class="site-branding">
+				<?php if ( is_active_sidebar( 'header-area' ) ) { ?>
+					<div class="sidebar-branding" role="complementary">
+						<?php dynamic_sidebar( 'header-area' ); ?>
+					</div><!-- #primary-sidebar -->
+				<?php } else { ?>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+				<?php } ?>
+			</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', '_s' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', '_s' ); ?></button>
+				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+			</nav><!-- #site-navigation -->
+		</div>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
