96957e61c0f8f552bc6e5698b4ff549f6b8cd1ef
devonhg <devon.hugh.godfrey@gmail.com>
2015-08-02 20:14:40 -0400

    1.2.10 Some CSS modifications.

---
+++ page-templates/page_temp-no-content.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Name: No Content
+ * Content free pages.
+ * 
+ * Select this template to represent the front page. 
+ *
+ * @package _s
+ */
+
+get_header(); ?>
+
+<?php get_footer(); ?>
